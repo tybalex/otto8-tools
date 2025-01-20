354de4ffb16dd9dbe62b02eb533d1948b5e5f631
Darren Shepherd <darren@acorn.io>
2025-01-20 10:07:15 -0700

    chore: shell enhancements

---
+++ docker/volumes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OBOT_VOLUMES=""
+HOMEVOL="${DATABASE_WORKSPACE_ID}"
+if [ -z "$HOMEVOL" ]; then
+    HOMEVOL="${GPTSCRIPT_WORKSPACE_ID}"
+fi
+
+if [ -n "$HOMEVOL" ]; then
+    HOMEVOLNAME="user-$(echo $HOMEVOL | shasum -a 256 | awk '{print $1}')"
+    OBOT_VOLUMES="${OBOT_VOLUMES} -v ${HOMEVOLNAME}:/home"
+fi
+
+DATADIR=$(echo "$GPTSCRIPT_WORKSPACE_ID" | sed 's!directory://!!')
+if [ -d "$DATADIR" ]; then
+    OBOT_VOLUMES="${OBOT_VOLUMES} -v ${DATADIR}/files:/mnt/data"
+fi
