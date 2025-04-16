796a3ca90c46b9207f8f66d188d5f51f30ed5cbb
Daishan Peng <StrongMonkey@users.noreply.github.com>
2025-04-16 13:22:01 -0700

    Fix: Add abilities to list other labels and select category (#588)
    Signed-off-by: Daishan Peng <daishan@acorn.io>

--- google/gmail/listInbox.py
+++ google/gmail/listInbox.py
@@ -8,7 +8,12 @@ async def main():
     max_results = os.getenv('MAX_RESULTS', '100')
     if max_results is not None:
         max_results = int(max_results)
-    query = 'label:inbox'
+    category = os.getenv('CATEGORY', 'primary')
+    label = os.getenv('LABEL', 'inbox')
+    if label != 'inbox':
+        query = f'label:{label}'
+    else:
+        query = f'label:{label} category:{category}'
 
     service = client('gmail', 'v1')
     await list_messages(service, query, max_results)
