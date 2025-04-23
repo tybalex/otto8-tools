a026340d7f1470c214df84f829143004d72ff5a8
Taylor Price <drpebcak@users.noreply.github.com>
2025-04-23 15:53:29 -0700

    fix: correct env var for excel tool (#612)

--- microsoft365/excel/pkg/global/global.go
+++ microsoft365/excel/pkg/global/global.go
@@ -1,6 +1,6 @@
 package global
 
-const CredentialEnv = "GPTSCRIPT_MICROSOFT_EXCEL_TOKEN"
+const CredentialEnv = "GPTSCRIPT_GRAPH_MICROSOFT_COM_BEARER_TOKEN"
 
 var (
 	ReadOnlyScopes = []string{"Files.Read", "User.Read"}
