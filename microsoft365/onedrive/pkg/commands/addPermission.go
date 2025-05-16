bcf92e40cf6569d0b4c203900574f71bc20cffec
Yingbei Tong <yingbei@acorn.io>
2025-05-15 18:45:57 -0700

    fix: remove invalid roles (#672)

--- microsoft365/onedrive/pkg/commands/addPermission.go
+++ microsoft365/onedrive/pkg/commands/addPermission.go
@@ -25,7 +25,7 @@ func AddPermission(ctx context.Context, driveID string, itemID string, emails st
 		}
 	}
 
-	validRoles := []string{"read", "write", "member", "owner"}
+	validRoles := []string{"read", "write"}
 	if !slices.Contains(validRoles, role) {
 		return fmt.Errorf("invalid role: %s, valid roles are: %s", role, strings.Join(validRoles, ", "))
 	}
